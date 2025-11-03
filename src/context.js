165250cbb71f0ada629a017e3b6c65cdd3ad5eac
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-11-03 14:09:09 +0530

    Project Stripe Submenu

--- src/context.js
+++ src/context.js
@@ -4,21 +4,26 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: '', links: [] });
 
     const openSidebar = () => {
-        setIsSidebarOpen(true)
+        setIsSidebarOpen(true);
     }
     const closeSidebar = () => {
-        setIsSidebarOpen(false)
+        setIsSidebarOpen(false);
     }
 
-    const openSubmenu = () => {
-        setIsSubmenuOpen(true)
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page)
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);
     }
     const closeSubmenu = () => {
-        setIsSubmenuOpen(false)
+        setIsSubmenuOpen(false);
     }
 
     return (
@@ -30,6 +35,8 @@ export const AppProvider = ({ children }) => {
                 openSidebar,
                 closeSubmenu,
                 closeSidebar,
+                location,
+                page,
             }}
         >
             {children}
